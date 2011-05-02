26eca8dec8f341a9958e72d68422c010483389ac
thanuz <alonso.minaya@gmail.com>
2011-05-02 04:28:56 -0500

    agregue funcion esqueleto para carros del gobierno

--- BCBA/grails-app/domain/bcba/GovernmentCar.groovy
+++ BCBA/grails-app/domain/bcba/GovernmentCar.groovy
@@ -1,4 +1,6 @@
 package bcba
+import java.io.File
+import jxl.*
 
 class GovernmentCar {
 
@@ -13,4 +15,29 @@ class GovernmentCar {
 
     static constraints = {
     }
+	def static uploadCreate(multipartfile){
+		
+		File file = new File('gov')
+		multipartfile.transferTo(file)
+		
+		Workbook workbook = Workbook.getWorkbook(file)
+		Sheet sheet = workbook.getSheet(0)
+		def rows = sheet.getRows()
+		
+		Cell year,make,model,discount,plate
+		
+		print 'El numero de filas es : '+rows
+		print '---------------------------------------'
+		for (i in 0..rows-1){
+			year = sheet.getCell(0,i)
+			make = sheet.getCell(1,i)
+			model = sheet.getCell(2,i)
+			discount = sheet.getCell(3,i)
+			plate = sheet.getCell(4,i)
+			
+			print 'año : '+year.getContents()+',creador: '+make.getContents()+',modelo: '+model.getContents()+',descuento: '+discount.getContents()+',placa: '+plate.getContents()+'\n'
+		}
+		print '---------------------------------------'	
+		file.delete()	
+	}
 }
