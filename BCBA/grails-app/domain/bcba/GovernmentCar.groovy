fb282a8985b8f4498a00ec8acb39c76f5b8c6d42
thanuz <alonso.minaya@gmail.com>
2011-05-03 00:35:23 -0500

    listo el agregar carros gobierno desde excel

--- BCBA/grails-app/domain/bcba/GovernmentCar.groovy
+++ BCBA/grails-app/domain/bcba/GovernmentCar.groovy
@@ -24,18 +24,53 @@ class GovernmentCar {
 		Sheet sheet = workbook.getSheet(0)
 		def rows = sheet.getRows()
 		
-		Cell year,make,model,discount,plate
+		Cell year,maker,model,discount,plate
 		
 		print 'El numero de filas es : '+rows
-		print '---------------------------------------'
-		for (i in 0..rows-1){
+		print '---------------------------------------\n'
+		for (i in 1..rows-1){
 			year = sheet.getCell(0,i)
-			make = sheet.getCell(1,i)
+			maker = sheet.getCell(1,i)
 			model = sheet.getCell(2,i)
 			discount = sheet.getCell(3,i)
 			plate = sheet.getCell(4,i)
 			
-			print 'año : '+year.getContents()+',creador: '+make.getContents()+',modelo: '+model.getContents()+',descuento: '+discount.getContents()+',placa: '+plate.getContents()+'\n'
+			def tmpGovCar = GovernmentCar.findByPlateNumber(plate.getContents())
+			print tmpGovCar
+			//verificar que el year sea válido
+			def yearTmp = 1980
+			if(year.getContents().isInteger()){
+
+				//De ser asi se obtiene el year
+				yearTmp = year.getContents().toInteger()
+			}
+			
+			if(tmpGovCar == null){
+				//si no esta hay que crearlo
+				//verificar el modelo 
+				
+				def tmpMaker = Maker.findByDescription(maker.getContents())
+				def tmpModel = CarModel.findByDescription(model.getContents())
+								
+				if(tmpModel == null){
+					if(tmpMaker == null){
+						def newMaker = new Maker(description:maker.getContents())
+						newMaker.save()
+						tmpMaker = newMaker
+					}
+
+					def newModel = new CarModel(description:model.getContents(),maker:tmpMaker)
+					newModel.save()
+					tmpModel = newModel
+					def newGovCar = new GovernmentCar(year:yearTmp,carModel:tmpModel,discount:false,plateNumber:plate.getContents())
+					newGovCar.save()
+				}else{
+					def newGovCar = new GovernmentCar(year:yearTmp,carModel:tmpModel,discount:false,plateNumber:plate.getContents())
+					newGovCar.save()
+				}
+				
+				print 'año : '+year.getContents()+',creador: '+maker.getContents()+',modelo: '+model.getContents()+',descuento: '+discount.getContents()+',placa: '+plate.getContents()+'\n'				
+			}
 		}
 		print '---------------------------------------'	
 		file.delete()	
