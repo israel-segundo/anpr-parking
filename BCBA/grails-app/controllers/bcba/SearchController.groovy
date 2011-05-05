92160a14d71c42ace8dfbe52412de8d425bc1592
Israel Segundo <israel.segundo@gmail.com>
2011-05-05 10:29:42 -0500

    modificacion que realiza busqueda cuando se registra llegada

--- BCBA/grails-app/controllers/bcba/SearchController.groovy
+++ BCBA/grails-app/controllers/bcba/SearchController.groovy
@@ -17,22 +17,29 @@ class SearchController {
 
     def consultPlate = {
 
-        def result = ''
+        def result = []
 
+        /* Hacer petición a cámara ip, mandarlo al módulo de reconocimiento*/
 //		def recognizer = new PlateRecognizer("http://192.168.1.114")
 //		def result = recognizer.processImage()
-
-
 //		result.put('discount', 'true')
 
         result = [
             'plate' : 'LXK1099',
             'url'   : 'http://localhost/fotos/C360_2011-04-21%2015-39-05.jpg',
             'status': 'ok', // ok, warning, error
-            'time'  : 1234,
-            'discount' : false
+            'time'  : 123
         ]
 
+        def discount = false
+
+        if( result['status'] != null && result['status'] == 'ok' ){
+            discount = GovernmentCar.isGreenCar( result['plate'] )
+        }
+
+        result.put( 'discount', discount)
+
+        /* guardar registro en bitácora */
         def pr          = new ParkingRecord(
             plateNumber  : result['plate'],
             entranceDate : new Date(),
@@ -41,7 +48,8 @@ class SearchController {
         )
         
         pr.save()
-        
+
+        /* Devolver datos en json*/
         def converter = result as JSON
 
 	render params.callback + "('" + converter.toString() + "')"
